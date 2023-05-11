223616683cf9a650de6fc80e5ee940cd30cc4b40
Aarti-developer <ingaleaarti956@gmail.com>
2023-05-11 18:15:56 +0530

    worked on home page view

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  featuredPostsArray:any;
+
+  constructor( private postService: PostsService ){ 
+    this.postService.loadFeatured().subscribe(val =>{
+      this.featuredPostsArray = val;
+      console.log(this.featuredPostsArray);
+    })
+  }
+  ngOnInit(): void {
+    
+  }
 }
---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+// import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  constructor(private afs: AngularFirestore) {}
+
+  loadFeatured() {
+    return this.afs
+      .collection('posts', ref => ref.where('isFeatured', '==', true) )
+      .snapshotChanges()
+      .pipe(
+        map((actions) => {
+          return actions.map((a) => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            return { id, data };
+          });
+        })
+      );
+  }
+}
